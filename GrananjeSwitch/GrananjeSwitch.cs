bf466b70a0387954d1f89668eb6d03ae580a6f5c
vanjs83 <tihomir.vanjurek@gmail.com>
2015-11-05 20:55:13 +0100

    GrananjeSwitch solve

--- GrananjeSwitch/GrananjeSwitch.cs
+++ GrananjeSwitch/GrananjeSwitch.cs
@@ -36,7 +36,24 @@ namespace Vsite.CSharp
                 case DayOfWeek.Monday:
                     return "ponedjeljak";
 
-                // TODO: Dopisati grane case za svaki dan u tjednu (DayOfWeek.Tuesday = Utorak, itd.)
+                case DayOfWeek.Tuesday:
+                    return "utorak";
+
+                case DayOfWeek.Wednesday:
+                    return "srijeda";
+
+                case DayOfWeek.Thursday:
+                    return "cetvrtak";
+
+                case DayOfWeek.Friday:
+                    return "petak";
+
+                case DayOfWeek.Saturday:
+                    return "subota";
+
+                case DayOfWeek.Sunday:
+                    return "nedjlja";
+                // Dopisati grane case za svaki dan u tjednu (DayOfWeek.Tuesday = Utorak, itd.)
 
                 default:
                     return "Neispravan dan u tjednu";
@@ -47,9 +64,21 @@ namespace Vsite.CSharp
         {
             switch (danUTjednu)
             {
-                // TODO: Dopisati grane case tako da za svaki radni dan u tjednu vraća "radni dan", a za subotu i nedjelju vraća "vikend"
+                // Dopisati grane case tako da za svaki radni dan u tjednu vraća "radni dan", a za subotu i nedjelju vraća "vikend"
                 case DayOfWeek.Monday:
+                 
+                case DayOfWeek.Tuesday:
+                    
+                case DayOfWeek.Wednesday:
+                    
+                case DayOfWeek.Thursday:
+                   
+                case DayOfWeek.Friday:
                     return "radni dan";
+
+                case DayOfWeek.Saturday:
+                case DayOfWeek.Sunday:
+                    return "vikend";
 
                 default:
                     return "Neispravan dan u tjednu";
