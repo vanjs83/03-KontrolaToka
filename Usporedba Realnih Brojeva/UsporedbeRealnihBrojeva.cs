20c2d26ee5e6c86691da7c826042e9c575db3147
vanjs83 <tihomir.vanjurek@gmail.com>
2015-11-05 20:32:16 +0100

    Usporedba realnih Brojeva solve!!

--- Usporedba Realnih Brojeva/UsporedbeRealnihBrojeva.cs
+++ Usporedba Realnih Brojeva/UsporedbeRealnihBrojeva.cs
@@ -7,15 +7,15 @@ namespace Vsite.CSharp
         static void Main(string[] args)
         {
             double tri = 3.0;
-
-            // TODO: provjeriti što će ispisati donja usporedba te popraviti ispitivanja da se dobije očekivani rezultat
-            if ((1.0 - 1.0 / tri) == (2.0 / tri))
+            double epsilon=1e-10;
+            // provjeriti što će ispisati donja usporedba te popraviti ispitivanja da se dobije očekivani rezultat
+            if (Math.Abs((1.0 - 1.0 / tri) - (2.0 / tri))<=epsilon)
                 Console.WriteLine("1 - 1/3 jednako je 2/3");
             else
                 Console.WriteLine("1 - 1/3 nije jednako 2/3!");
 
-            // TODO: provjeriti što će ispisati donja usporedba te popraviti ispitivanja da se dobije očekivani rezultat
-            if (tri * 0.1 == 0.3)
+            // provjeriti što će ispisati donja usporedba te popraviti ispitivanja da se dobije očekivani rezultat
+            if (Math.Abs((tri * 0.1 - 0.3)) <= epsilon)
                 Console.WriteLine("3 * 0.1 je jednako 0.3");
             else
                 Console.WriteLine("3 * 0.1 nije jednako 0.3!");
