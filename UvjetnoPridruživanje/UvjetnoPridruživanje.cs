42b42bbfece3f89d36030617a35b5759948d0650
vanjs83 <tihomir.vanjurek@gmail.com>
2015-11-05 20:42:30 +0100

    UvjetnoPridruživanje solve

--- UvjetnoPridruživanje/UvjetnoPridruživanje.cs
+++ UvjetnoPridruživanje/UvjetnoPridruživanje.cs
@@ -13,13 +13,17 @@ namespace Vsite.CSharp
             {
                 int broj = int.Parse(unos);
 
-                // TODO: Napisati uvjetno pridruživanje ?: koje će ovisno o dijeljivosti broja varijabli odgovor dodijeliti vrijednost "paran" ili "neparan"
-                string odgovor = "paran";
-
+                // Napisati uvjetno pridruživanje ?: koje će ovisno o dijeljivosti broja varijabli odgovor dodijeliti vrijednost "paran" ili "neparan"
+                string odgovor;
+                odgovor=broj % 2==0 ? "paran" : "neparan";
                 Console.WriteLine("Broj {0} je {1}", broj, odgovor);
 
-                // TODO: Napisati isto pridruživanje ali korištenjem uvjetnog grananja if
-                string odgovor2 = "paran";
+                // Napisati isto pridruživanje ali korištenjem uvjetnog grananja if
+                string odgovor2="paran" ;
+                if (broj % 2!= 0)
+                    odgovor2 = "neparan";
+                
+                
 
                 Console.WriteLine("Broj {0} je {1}", broj, odgovor2);
 
